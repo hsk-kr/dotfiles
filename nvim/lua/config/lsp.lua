39462419eab6372f5244763bc413c873918200a7
hsk-kr <hsk.coder@gmail.com>
2024-08-24 13:52:12 +0100

    tailwind plugin

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -14,7 +14,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'tsserver'},
+  ensure_installed = {'tsserver', 'tailwindcss'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
