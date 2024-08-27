ffc12bfec9151908481e7de4eefd742bc46b2f17
Seongkuk Han <seongkuk.han@janustechnology.co.uk>
2024-08-27 13:36:50 +0100

    ignorecase true by default

--- nvim/lua/config/default.lua
+++ nvim/lua/config/default.lua
@@ -1,9 +1,12 @@
 --vim.wo.relativenumber = true
-vim.o.number = true
 vim.cmd.colorscheme "catppuccin"
+
+vim.o.number = true
+vim.o.ignorecase = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.smartindent = true
+
 vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, {})
 vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, {})
 vim.keymap.set('n', '<leader>de', function() vim.diagnostic.open_float() end, { noremap = true, silent = true })
