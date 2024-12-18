9f4121dd089007b1e4d0d1561bf9a07e47d646a3
hsk-kr <hsk.coder@gmail.com>
2024-12-18 15:50:31 +0000

    add vim-go and format on save

--- nvim/lua/config/format.lua
+++ nvim/lua/config/format.lua
@@ -31,6 +31,15 @@ null_ls.setup({
   end,
 })
 
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    vim.cmd('GoFmt')
+  end,
+  group = format_sync_grp,
+})
+
 local prettier = require("prettier")
 
 prettier.setup({
