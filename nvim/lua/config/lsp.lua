6c4b40b83c3428134f6740108118a6cc6aa1f11d
hsk-kr <hsk.coder@gmail.com>
2024-12-18 16:30:32 +0000

    change cmp to blink.cmp
    need change ctrl-y, for some reasons, option selection keys in the docs
    not working

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -66,41 +66,45 @@ require('mason-lspconfig').setup({
   }
 })
 
-local cmp = require('cmp')
+local capabilities = require('blink.cmp').get_lsp_capabilities()
+local lspconfig = require('lspconfig')
+lspconfig['lua-ls'].setup({ capabilities = capabilities })
 
-cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  mapping = {
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    },
-    -- ['<C-.>'] = cmp.mapping.confirm({select = false}),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-    ['<C-p>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        cmp.complete()
-      end
-    end),
-    ['<C-n>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        cmp.complete()
-      end
-    end),
-  },
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-})
+-- local cmp = require('cmp')
+--
+-- cmp.setup({
+--   sources = {
+--     {name = 'nvim_lsp'},
+--   },
+--   mapping = {
+--     ["<CR>"] = cmp.mapping.confirm {
+--       behavior = cmp.ConfirmBehavior.Insert,
+--       select = true,
+--     },
+--     -- ['<C-.>'] = cmp.mapping.confirm({select = false}),
+--     ['<C-e>'] = cmp.mapping.abort(),
+--     ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+--     ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+--     ['<C-p>'] = cmp.mapping(function()
+--       if cmp.visible() then
+--         cmp.select_prev_item()
+--       else
+--         cmp.complete()
+--       end
+--     end),
+--     ['<C-n>'] = cmp.mapping(function()
+--       if cmp.visible() then
+--         cmp.select_next_item()
+--       else
+--         cmp.complete()
+--       end
+--     end),
+--   },
+--   snippet = {
+--     expand = function(args)
+--       require('luasnip').lsp_expand(args.body)
+--     end,
+--   },
+-- })
 
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition);
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
 	{"williamboman/mason.nvim", dependencies = {"stevearc/dressing.nvim"}},
 	{"williamboman/mason-lspconfig.nvim"},
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
+	{'neovim/nvim-lspconfig'}
+	-- {'hrsh7th/cmp-nvim-lsp'},
+	-- {'hrsh7th/nvim-cmp'},
 }
