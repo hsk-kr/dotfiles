6d962b054b7ee70aa11010a0dcf3298af2425717
hsk-kr <hsk.coder@gmail.com>
2024-12-20 11:10:34 +0000

    fix tmux scheme problems and change diag icons

--- nvim/lua/config/default.lua
+++ nvim/lua/config/default.lua
@@ -1,5 +1,4 @@
 vim.wo.relativenumber = true
-vim.cmd.colorscheme("catppuccin")
 
 vim.filetype.add({
 	extension = {
@@ -45,3 +44,26 @@ set("n", "<leader>de", function()
 end, { noremap = true, silent = true })
 
 set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { silent = true })
+
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+		linehl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+		},
+	},
+})
