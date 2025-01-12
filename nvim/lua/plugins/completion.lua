e93b7811774a5ce06d129c126a52379d42cc0d94
hsk-kr <hsk.coder@gmail.com>
2025-01-12 23:45:45 +0000

    update deps, blink config

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,15 +1,12 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
-  opts_extend = {
-    "sources.default",
-  },
-  version = "v0.*",
+  'saghen/blink.cmp',
+  dependencies = 'rafamadriz/friendly-snippets',
+  version = '*',
   opts = {
-    keymap = { preset = "default" },
+    keymap = { preset = 'default' },
     appearance = {
       use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
+      nerd_font_variant = 'mono'
     },
     completion = {
       accept = { auto_brackets = { enabled = true } },
@@ -25,21 +22,12 @@ return {
         border = "single",
       },
       list = {
-        selection = "auto_insert",
+        selection = { auto_insert = true },
       },
     },
     sources = {
-      default = { "lsp", "path", "luasnip", "snippets", "buffer", "lazydev" },
-      cmdline = {},
-      providers = {
-        lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
-        buffer = {
-          name = "Buffer",
-          module = "blink.cmp.sources.buffer",
-          min_keyword_length = 3,
-        },
-      },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
-    signature = { enabled = true },
   },
+  opts_extend = { "sources.default" }
 }
