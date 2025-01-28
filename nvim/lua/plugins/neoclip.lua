9e0ca9b0d5c2d08d1a9fb448889082d4c76d847b
hsk-kr <hsk.coder@gmail.com>
2025-01-28 15:48:25 +0000

    add nvim-clip persistent

--- nvim/lua/plugins/neoclip.lua
+++ nvim/lua/plugins/neoclip.lua
@@ -4,11 +4,13 @@ return {
 		dependencies = {
 			-- you'll need at least one of these
 			{ "nvim-telescope/telescope.nvim" },
+			{ "kkharji/sqlite.lua", module = "sqlite" },
 			-- {'ibhagwan/fzf-lua'},
 		},
 		config = function()
 			require("neoclip").setup({
 				history = 100,
+				enable_persistent_history = true,
 			})
 		end,
 	},
