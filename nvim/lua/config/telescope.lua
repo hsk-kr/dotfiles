73060c0f91404f0dd7d3996fcf15b634507ba600
hsk.coder <hsk.coder@gmail.com>
2025-03-19 23:29:10 +0000

    nvim: cff command search for .config not dotfiles

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -3,7 +3,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 -- live_grep_args
 vim.keymap.set("n", "<leader>fg", function()
-	require("telescope").extensions.live_grep_args.live_grep_args()
+  require("telescope").extensions.live_grep_args.live_grep_args()
 end, { desc = "Live Grep Args" })
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
@@ -11,17 +11,17 @@ vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- setup
 vim.keymap.set("n", "<leader>cff", function()
-	builtin.find_files({ cwd = "~/.config/dotfiles", prompt_title = " dotfile" })
-end, { desc = "Search neovim files" })
+  builtin.find_files({ cwd = "~/.config", prompt_title = " config" })
+end, { desc = "Search config files" })
 vim.keymap.set("n", "<leader>cfg", function()
-	builtin.live_grep({ cwd = "~/.config/dotfiles", prompt_title = " dotfile" })
-end, { desc = "Search neovim files" })
+  builtin.live_grep({ cwd = "~/.config", prompt_title = " config" })
+end, { desc = "Search config files" })
 
 -- add a mapping for git status
 vim.keymap.set("n", "<leader>gs", function()
-	builtin.git_status({})
+  builtin.git_status({})
 end, { desc = "Git status" })
 
 vim.keymap.set("n", "<leader>gb", function()
-	builtin.git_branches({})
+  builtin.git_branches({})
 end, { desc = "Git branches" })
