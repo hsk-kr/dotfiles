810cf8c48aa88887cbdc9ea235f684357ff317f4
hsk-kr <hsk.coder@gmail.com>
2025-03-20 10:40:14 +0000

    nvim: add codecompanion

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,37 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("codecompanion").setup({
+			adapters = {
+				anthropic = function()
+					return require("codecompanion.adapters").extend("anthropic", {
+						name = "anthropic",
+						schema = {
+							model = {
+								default = "claude-3-5-haiku-20241022",
+							},
+							num_ctx = {
+								default = 16384,
+							},
+							num_predict = {
+								default = -1,
+							},
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					adapter = "anthropic",
+				},
+				inline = {
+					adapter = "anthropic",
+				},
+			},
+		})
+	end,
+}
