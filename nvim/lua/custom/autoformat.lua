93b7920979180be74e5f342b4d591ed2794d0581
hsk-kr <hsk.coder@gmail.com>
2025-04-17 16:57:24 +0100

    nvim: auto format setup

--- nvim/lua/custom/autoformat.lua
+++ nvim/lua/custom/autoformat.lua
@@ -1,32 +1,32 @@
 local setup = function()
-  -- Autoformatting setup
+	-- Autoformatting setup
 
-  local conform = require "conform"
-  conform.setup {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      typescript = { "prettierd", "deno_fmt", stop_after_first = true },
-      typescriptreact = { "prettierd", "deno_fmt", stop_after_first = true },
-      javascript = { "prettierd" },
-      markdown = { "prettierd" },
-      json = { "prettierd" },
-      yaml = { "prettierd" },
-      elm = { "elm_format" },
-      ruby = { "rubocop" },
-      go = { "gofmt", "goimports" },
-    },
-  }
+	local conform = require("conform")
+	conform.setup({
+		formatters_by_ft = {
+			lua = { "stylua" },
+			typescript = { "prettierd", "deno_fmt", stop_after_first = true },
+			typescriptreact = { "prettierd", "deno_fmt", stop_after_first = true },
+			javascript = { "prettierd" },
+			markdown = { "prettierd" },
+			json = { "prettierd" },
+			yaml = { "prettierd" },
+			elm = { "elm_format" },
+			ruby = { "rubocop" },
+			go = { "gofmt", "goimports" },
+		},
+	})
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = vim.api.nvim_create_augroup("custom-conform", { clear = true }),
-    callback = function(args)
-      require("conform").format {
-        bufnr = args.buf,
-        lsp_fallback = true,
-        quiet = true,
-      }
-    end,
-  })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		group = vim.api.nvim_create_augroup("custom-conform", { clear = true }),
+		callback = function(args)
+			require("conform").format({
+				bufnr = args.buf,
+				lsp_fallback = true,
+				quiet = true,
+			})
+		end,
+	})
 end
 
 setup()
