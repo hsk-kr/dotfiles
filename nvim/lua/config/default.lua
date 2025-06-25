efcbf90c90b4d2871241fefa8c924d291661e6eb
hsk-kr <hsk.coder@gmail.com>
2025-06-25 12:02:27 +0100

    nvim: store current session when quitting

--- nvim/lua/config/default.lua
+++ nvim/lua/config/default.lua
@@ -62,6 +62,36 @@ vim.api.nvim_set_keymap("n", "<leader>upt", ":LicovimTestRunnerToggle<CR>", { no
 
 vim.opt.clipboard = "unnamedplus"
 
+-- Auto save session on exit
+vim.api.nvim_create_autocmd("VimLeavePre", {
+	callback = function()
+		if _G.saving_mode == true then
+			vim.cmd("mksession! ~/vim_saving_mode_session.vim")
+			-- TODO: not working, this command should be run manually, later need to fix
+			-- vim.cmd("bufdo filetype detect")
+		end
+	end,
+})
+
+-- Auto load session on start
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local session_file = vim.fn.expand("~/vim_saving_mode_session.vim")
+		if vim.fn.filereadable(session_file) == 1 then
+			_G.saving_mode = false
+			vim.cmd("source " .. session_file)
+			vim.fn.delete(session_file)
+		end
+	end,
+})
+
+vim.api.nvim_create_user_command("QuitVimWithSavingMode", function()
+	_G.saving_mode = true
+	vim.cmd("qa!")
+end, {})
+
+vim.api.nvim_set_keymap("n", "<leader>sq", ":QuitVimWithSavingMode<CR>", { noremap = true, silent = true })
+
 -- key mapping
 local set = vim.keymap.set
 
