ed789b90c1c93c0b37d6cb09afef073840b9d0af
hsk-kr <hsk.coder@gmail.com>
2025-10-04 14:11:37 +0100

    nvim: blink for completion

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -7,18 +7,5 @@ vim.opt.winborder = "rounded"
 local lsp_languages = { "typescript", "lua", "go" }
 
 for _, lsp_language in pairs(lsp_languages) do
-	vim.lsp.enable(lsp_language)
+  vim.lsp.enable(lsp_language)
 end
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method("textDocument/completion") then
-			vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-			vim.keymap.set("i", "<C-Space>", function()
-				vim.lsp.completion.get()
-			end)
-		end
-	end,
-})
