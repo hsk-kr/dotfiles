f232d96575a5c71e7697771edf405b1b34fbc871
hsk-kr <hsk.coder@gmail.com>
2025-10-08 11:05:30 +0100

    nvim: change blink and diagnostics configs

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,11 +1,20 @@
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true })
 vim.keymap.set("n", "gk", vim.lsp.buf.hover, { noremap = true, silent = true })
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { noremap = true, silent = true })
 
 vim.opt.winborder = "rounded"
 
 local lsp_languages = { "typescript", "lua", "go" }
 
 for _, lsp_language in pairs(lsp_languages) do
-  vim.lsp.enable(lsp_language)
+	vim.lsp.enable(lsp_language)
 end
+
+vim.diagnostic.config({
+	virtual_text = true, -- show inline messages
+	signs = true, -- show signs in the gutter
+	underline = true, -- underline problematic text
+	update_in_insert = false, -- don't update diagnostics while typing
+	severity_sort = true, -- sort diagnostics by severity
+})
